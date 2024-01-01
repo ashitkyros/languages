543efefedc5d27c1386c3650877698a0f84ec16d
Ashit <ashit.kyros@gmail.com>
2024-01-01 17:53:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
 
+const languages = [
+  {
+    code: "ar",
+    name: "العربية",
+    country_code: "sa",
+    dir: "rtl"
+  },
+  {
+    code: "en",
+    name: "English",
+    country_code: "gb",
+  },
+  {
+    code: "guj",
+    name: "ગુજરાતી",
+    country_code: "guj",
+  },
+];
 function App() {
+  const currentLanguageCode = Cookies.get("i18next") || "en";
+  const currentLanguage = languages.find((l) => l.code === currentLanguageCode);
+  const { t } = useTranslation();
+  const releaseDate = new Date("2021-03-07");
+  const timeDifference = new Date() - releaseDate;
+  const number_of_days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+  useEffect(() => {
+    document.body.dir = currentLanguage.dir || "ltr";
+    document.title= t('app_title')
+  }, [currentLanguage, t]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="d-flex justify-content-end">
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Dropdown button
+          </button>
+          <ul className="dropdown-menu">
+            <li> <span className="dropdown-item-text">{t('language')}</span></li>
+            {languages.map(({ code, name, country_code }) => (
+              <li key={country_code}>
+                <button
+                  className="dropdown-item"
+                  onClick={() => i18next.changeLanguage(code)}
+                  disabled={code === currentLanguageCode}
+                >
+                  <span
+                    className={`flag-icon flag-icon-${country_code} mx-2`}
+                  ></span>
+                  {name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="d-flex flex-column align-items-start">
+        <h1 className="font-weight-normal mb-3">{t("welcome_message")}</h1>
+        <p>{t("days_since_release", { number_of_days })}</p>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportesLng: ["ar", "en", "guj"],
+    fallbackLng: "en",
+    detection: {
+      order: ["path", "cookie", "htmlTag", "localStorage", "subdomain"],
+      caches: ["cookie"],
+    },
+    backend: {
+      loadPath: "./assets/locales/{{lng}}/translation.json",
+    },
+  });
+
+const lodingMarkup = (
+  <div className="py-4 text-center">
+    <h2>Loading...</h2>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Suspense fallback={lodingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>
 );
 
 // If you want to start measuring performance in your app, pass a function
